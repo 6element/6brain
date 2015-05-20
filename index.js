4024fa000a421948b459ce290cc64b855ba9b609
Alexandre Vallette <vallettea@gmail.com>
2015-05-20 16:32:15 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+"use strict;"
+
+
+var sensor = require("6sense");
+var encodeForSMS = require('6sense/js/codec/encodeForSMS.js');
+var quipu = require("quipu");
+var numbers = require("./numbers.json");
+quipu.handle("initialize", "/dev/ttyUSB0");
+
+sensor.record();
+
+sensor.on('results', function(results){
+        console.log('ready to send SMS');
+        console.log('results', results);
+
+        encodeForSMS(results).then(function(sms){
+            quipu.handle("sendSMS", sms, numbers.serverNumber);
+        });
+});
