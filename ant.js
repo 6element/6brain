db13ddddaf22a658431829a9b03d08af4379510d
Alexandre Vallette <vallettea@gmail.com>
2015-06-29 16:18:34 +0200

    two ways of communication

--- ant.js
+++ ant.js
@@ -4,6 +4,7 @@
 var sensor = require("6sense");
 var encodeForSMS = require('6sense/src/codec/encodeForSMS.js');
 var quipu = require("quipu");
+var parser = require("quipu/parser.js")
 var schedule = require('node-schedule');
 var PRIVATE = require("./PRIVATE.json");
 
@@ -13,7 +14,7 @@ var os = require("os");
 
 var MEASURE_PERIOD = 300; // in seconds
 var WAKEUP_HOUR_UTC = "07";
-var SLEEP_HOUR_UTC = "18";
+var SLEEP_HOUR_UTC = "16";
 var DEBUG = process.env.DEBUG ? process.env.DEBUG : false;
 
 var devices = {
@@ -24,6 +25,7 @@ var devices = {
 var antName = os.hostname();
 var lastSender = undefined;
 var lastCommandArgs = undefined;
+var forServer = undefined;
 var firstInit = true;
 var shouldTunnel = false;
 
@@ -34,10 +36,24 @@ var debug = function() {
     };
 }
 var sendSMS = function(encoded, body, dest){
-   if (encoded === "encoded")
+   if (encoded === "6sense_encoded")
       quipu.sendSMS("1" + body, dest);
+   else if (encoded === "generic_encoded")
+      quipu.sendSMS("2" + body, dest);
    else
       quipu.sendSMS("0" + body, dest);
+}
+var sendResponseAndStatus = function(query, result){
+   var body = {
+      "info": 
+         {"command": query, "result": result}, 
+      "quipu": quipu.state, 
+      "6sense": sensor.state
+   };
+   parser.encode(body)
+      .then(function(message){
+         quipu.sendSMS("generic_encoded", message, lastSender);
+      })
 }
 // initialize communication
 quipu.handle("initialize", devices, PRIVATE.PIN);
@@ -45,7 +61,7 @@ quipu.on("transition", function (data){
    if (data.toState === "initialized" && firstInit){
       firstInit = false;
       sendSMS("clear", "init", PRIVATE.serverNumber);
-      sendSMS("clear", "initialization of " + antName, PRIVATE.installerNumber);
+      sendSMS("clear", "initialization of " + antName, PRIVATE.authorizedNumbers[0]);
    }
 });
 
@@ -72,15 +88,15 @@ schedule.scheduleJob('00 '+ WAKEUP_HOUR_UTC +' * * *', function(){
 
 quipu.on("transition", function (data){
    if (data.toState === "3G_connected" && data.fromState === "tunnelling")
-      sendSMS("clear", "closedTunnel", lastSender);
+      sendResponseAndStatus("closetunnel", "OK");
    if (data.toState === "initialized" && data.fromState === "tunnelling")
-      sendSMS("clear", "closedTunnel and 3G", lastSender);
+      sendResponseAndStatus("close3g", "OK");
    if (data.toState === "tunnelling" && data.fromState === "3G_connected")
-      sendSMS("clear", "openedTunnel", lastSender);
+      sendResponseAndStatus("openTunnel", "OK");
    if (data.toState === "initialized" && data.fromState === "3G_connected")
-      sendSMS("clear", "3G_disconnected", lastSender);
+      sendResponseAndStatus("close3g", "OK");
    if (data.toState === "3G_connected" && data.fromState === "initialized") {
-      sendSMS("clear", "3G_connected", lastSender);
+      sendResponseAndStatus("open3g", "OK");
       if (shouldTunnel)
          quipu.handle("openTunnel", parseInt(lastCommandArgs[1]), parseInt(lastCommandArgs[2]), lastCommandArgs[3]);
    }
@@ -89,94 +105,106 @@ quipu.on("transition", function (data){
 quipu.on("tunnelError", function(msg){
    debug("tunnelError");
    quipu.handle("close3G");
-   sendSMS("clear", "closing 3G because error in tunneling: " + msg, lastSender);
+   sendResponseAndStatus("opentunnel", "ERROR: " + msg);
 });
 
 // receiving SMS, parse to make action
 quipu.on("smsReceived", function(sms){
    debug("SMS received: ", sms);
    var commandArgs = sms.body.trim().toLowerCase().split(":");
-   lastSender = sms.from;
-   debug("commandArgs ", commandArgs);
+   if (PRIVATE.authorizedNumbers.indexOf(sms.from) >= 0 || sms.from === PRIVATE.serverNumber) {
+      if (sms.from === PRIVATE.serverNumber) {
+         forServer = true;
+         lastSender = sms.from;
+      } else {
+         forServer = false;
+      }
+      debug("commandArgs ", commandArgs);
 
-   switch(commandArgs.length) {
+      switch(commandArgs.length) {
 
-      case 1:
-         // command with no parameter
-         var command = commandArgs[0];
+         case 1:
+            // command with no parameter
+            var command = commandArgs[0];
 
-         switch(command) {
-            case "status":
-               var response = "quipu:"+ quipu.state + " sense:"+ sensor.state + " signal:" + quipu.signalStrength;
-               sendSMS("clear", response, sms.from);
-               break;
-            case "reboot":
-               spawn("reboot");
-               break;
-            case "ip":
-               var ips = getIp();
-               var response = Object.keys(ips).map(function(k){return k+" : "+ips[k]}).join("  ");
-               sendSMS("clear", response, sms.from);
-               break;
-            case "resumerecord":
-               sensor.record(MEASURE_PERIOD);
-               break;
-            case "pauserecord":
-               sensor.pause();
-               break;
-            case "open3g":
-               quipu.handle("open3G");
-               break;
-            case "close3g":
-               shouldTunnel = false;
-               quipu.handle("close3G");
-               break;
-            case "closetunnel":
-               shouldTunnel = false;
-               quipu.handle("close3G");
-               break;
-            }
-         break;
-
-      case 2:
-         // command with two parameters
-         switch(commandArgs[0]) {
-            case "changeperiod":
-               if (commandArgs[1] == parseInt(commandArgs[1], 10)){
-                  MEASURE_PERIOD = parseInt(commandArgs[1], 10);
-                  sensor.pause();
-                  setTimeout(function(){
-                     sensor.record(MEASURE_PERIOD);
-                  }, 3000)
-               } else {
-                  console.log("Period is not an integer ", commandArgs[1])
-               }
-               break;
-         }
-         break;
-      case 4:
-         // command with four parameters
-         switch(commandArgs[0]) {
-            case "date":
-               var date = commandArgs[1].replace("t", " ") + ":" + commandArgs[2] + ":" + commandArgs[3].split(".")[0];
-               debug("Received date", sms.body, date);
-               spawn("timedatectl", ["set-time", date]);
-               // check if current time is valid and record consequently
-               var current_hour = new Date().getHours();
-               console.log("current_hour ", current_hour)
-               if (current_hour <= SLEEP_HOUR_UTC && current_hour >= WAKEUP_HOUR_UTC)
+            switch(command) {
+               case "status":
+                  if (forServer)
+                     sendResponseAndStatus(command, "OK");
+                  else
+                     sendSMS("clear", " quipu: " + quipu.state + " 6sense: "+ sensor.state, sms.from);
+                  break;
+               case "reboot":
+                  spawn("reboot");
+                  break;
+               case "ip":
+                  var ips = getIp();
+                  var response = Object.keys(ips).map(function(k){return k+" : "+ips[k]}).join("  ");
+                  sendSMS("clear", response, sms.from);
+                  break;
+               case "resumerecord":
                   sensor.record(MEASURE_PERIOD);
-               break;
-            case "opentunnel":
-               shouldTunnel = true;
-               lastCommandArgs = commandArgs;
-               quipu.handle("open3G");
-               break;
-         }
-         break;
-    
-      default:
-         console.log("Unrecognized command.", commandArgs)
-     
+                  sendResponseAndStatus(command, "OK");
+                  break;
+               case "pauserecord":
+                  sensor.pause();
+                  sendResponseAndStatus(command, "OK");
+                  break;
+               case "open3g":
+                  quipu.handle("open3G");
+                  break;
+               case "close3g":
+                  shouldTunnel = false;
+                  quipu.handle("close3G");
+                  break;
+               case "closetunnel":
+                  shouldTunnel = false;
+                  quipu.handle("close3G");
+                  break;
+               }
+            break;
+
+         case 2:
+            // command with two parameters
+            switch(commandArgs[0]) {
+               case "changeperiod":
+                  if (commandArgs[1] == parseInt(commandArgs[1], 10)){
+                     MEASURE_PERIOD = parseInt(commandArgs[1], 10);
+                     sensor.pause();
+                     setTimeout(function(){
+                        sensor.record(MEASURE_PERIOD);
+                     }, 3000)
+                  } else {
+                     console.log("Period is not an integer ", commandArgs[1])
+                  }
+                  break;
+            }
+            break;
+         case 4:
+            // command with four parameters
+            switch(commandArgs[0]) {
+               case "date":
+                  var date = commandArgs[1].replace("t", " ") + ":" + commandArgs[2] + ":" + commandArgs[3].split(".")[0];
+                  debug("Received date", sms.body, date);
+                  spawn("timedatectl", ["set-time", date]);
+                  // check if current time is valid and record consequently
+                  var current_hour = new Date().getHours();
+                  if (current_hour <= SLEEP_HOUR_UTC && current_hour >= WAKEUP_HOUR_UTC)
+                     sensor.record(MEASURE_PERIOD);
+                  break;
+               case "opentunnel":
+                  shouldTunnel = true;
+                  lastCommandArgs = commandArgs;
+                  quipu.handle("open3G");
+                  break;
+            }
+            break;
+       
+         default:
+            console.log("Unrecognized command.", commandArgs)
+        
+      }
+   } else {
+      console.log("Unauthorized number.");
    }
 });
