6676538b391cdc2cd2b19f44b3e8671fa07498c6
4rzael <maxime.agor@epitech.eu>
2015-12-16 10:43:57 +0100

    cleaned the getSimId code

--- install_scripts/getSimId.js
+++ install_scripts/getSimId.js
@@ -2,42 +2,43 @@
 
 var exec = require('child_process').exec;
 var path = require('path');
+var fs = require('fs');
 
 var SP = require('serialport');
 var SerialPort = SP.SerialPort;
 
 var privatePath = path.join(__dirname, '..', 'PRIVATE.json');
+var privateJson = require(privatePath);
 
-exec('cat ' + privatePath + ' | grep -e \'"sim": "undefined"\'', function (err, stdout) {
-	if (err || stdout.length === 0)
-		throw new Error('Cannot find an undefined field in the PRIVATE.json');
-	else {
+if (!privateJson)
+	throw new Error('Error in PRIVATE.json');
+if (!privateJson.sim) {
+	var serialPort = new SerialPort('/dev/serial/by-id/usb-HUAWEI_HUAWEI_HiLink-if02-port0', {
+		baudrate: 9600,
+		parser: SP.parsers.readline('\r')
+	});
 
-		var serialPort = new SerialPort('/dev/serial/by-id/usb-HUAWEI_HUAWEI_HiLink-if02-port0', {
-			baudrate: 9600,
-			parser: SP.parsers.readline('\r')
+	serialPort.on('data', function (data) {
+		if (typeof data !== 'string')
+			data = data.toString();
+
+		if (data.match('ICCID') && data.match('ERROR'))
+			console.log('error received from the modem');
+		else if (data.match(/ICCID: (.*)/)) {
+			var simId = data.match(/ICCID: (.*)/)[1];
+
+			console.log('SIM ID :', simId);
+			privateJson.sim = simId;
+			fs.writeFile(privatePath, JSON.stringify(privateJson), process.exit);
+		}
+	});
+
+	serialPort.on('open', function () {
+		console.log('modem port opened');
+
+		serialPort.write('AT^ICCID?\r', function (err) {
+			if (err)
+				console.log('error writing to the modem');
 		});
-
-		serialPort.on('data', function (data) {
-			if (typeof data !== 'string')
-				data = data.toString();
-
-			if (data.match('ICCID') && data.match('ERROR'))
-				console.log('error received from the modem');
-			else if (data.match(/ICCID: (.*)/)) {
-				console.log('SIM ID :', data.match(/ICCID: (.*)/)[1]);
-				exec('cat ' + privatePath + ' | sed \'s/"sim": "undefined"/"sim": "' + data.match(/ICCID: (.*)/)[1].trim() + '"/g\' > '+privatePath+'.tmp && mv '+privatePath+'.tmp ' + privatePath, process.exit);
-			}
-		});
-
-		serialPort.on('open', function () {
-			console.log('modem port opened');
-
-			serialPort.write('AT^ICCID?\r', function (err) {
-				if (err)
-					console.log('error writing to the modem');
-			});
-		});
-	}
-});
-
+	});
+}
